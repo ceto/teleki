ce16425741e096e4002e59a33975817e371d2272
Szabó Gábor <szabogabi@gmail.com>
2017-09-08 15:35:38 +0200

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,18 @@
+/* ========================================================================
+ * .noConflict()
+ * The routing is enclosed within an anonymous function so that you can
+ * always reference jQuery with $, even when in .noConflict() mode.
+ * ======================================================================== */
+
+(function($) {
+
+  $(document).foundation();
+
+  $(document).ready(function(){
+
+  });
+
+
+
+
+})(jQuery); // Fully reference jQuery after this point.
---
+++ lib/custom.php
@@ -0,0 +1,20 @@
+<?php
+
+  // 1. customize ACF path
+  add_filter('acf/settings/path', 'teleki_acf_settings_path');
+   function teleki_acf_settings_path( $path ) {
+      $path = get_stylesheet_directory() . '/lib/acf/';
+      return $path;
+  }
+  // 2. customize ACF dir
+  add_filter('acf/settings/dir', 'teleki_acf_settings_dir');
+  function teleki_acf_settings_dir( $dir ) {
+    $dir = get_stylesheet_directory_uri() . '/lib/acf/';
+    return $dir;
+  }
+
+  // 3. Hide ACF field group menu item
+  //add_filter('acf/settings/show_admin', '__return_false');
+
+  // 4. Include ACF
+  include_once( get_stylesheet_directory() . '/lib/acf/acf.php' );
