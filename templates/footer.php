9e6315e5c9031044337343be6fe4ac63de43726d
Szabó Gábor <szabogabor@hydrogene.hu>
2017-09-20 11:28:07 +0200

    itthonrol

--- templates/footer.php
+++ templates/footer.php
@@ -1,24 +1,24 @@
 <footer class="sitefooter" role="contentinfo">
-  <div class="grid-container sitefooter__main">
-    <div class="grid-x grid-margin-x large-up-3">
-        <?php dynamic_sidebar('sidebar-footer'); ?>
-        <div class="cell">
-            <section class="widget widget--footer widget-social">
-                <h3 class="widget__title">Kövess minket</h3>
-                <div class="widget__body">
-                    <p>Légy naprakész! Közösségi csatornáinkon aktualitásokról és eseményekről a leggyorsabban értesülhetsz.</p>
-                    <ul>
-                        <li><a target="_blank" href="#">Facebook</a></li>
-                        <li><a target="_blank" href="#">Instagram</a></li>
-                    </ul>
-                </div>
-            </section>
+    <div class="grid-container sitefooter__main">
+        <div class="grid-x grid-margin-x large-up-3">
+            <?php dynamic_sidebar('sidebar-footer'); ?>
+            <div class="cell">
+                <section class="widget widget--footer widget-social">
+                    <h3 class="widget__title">Kövess minket</h3>
+                    <div class="widget__body">
+                        <p>Légy naprakész! Közösségi csatornáinkon aktualitásokról és eseményekről a leggyorsabban értesülhetsz.</p>
+                        <ul>
+                            <li><a target="_blank" href="#">Facebook</a></li>
+                            <li><a target="_blank" href="#">Instagram</a></li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
         </div>
     </div>
-  </div>
-  <div class="grid-container sitefooter__sub">
-    <div class="grid-x grid-margin-x">
-        <div class="cell"><p>&copy; <?= date('Y') ?> BGSZC Teleki Blanka Közg. SZKI. - Minden jog fenntartva! &middot; <a href="#">Adatvédelem</a> &middot; <a href="#">Jogi nyilatkozat</a></p></div>
+    <div class="grid-container sitefooter__sub">
+        <div class="grid-x grid-margin-x">
+            <div class="cell"><p>&copy; <?= date('Y') ?> BGSZC Teleki Blanka Közg. SZKI. - Minden jog fenntartva! &middot; <a href="#">Adatvédelem</a> &middot; <a href="#">Jogi nyilatkozat</a></p></div>
+        </div>
     </div>
-  </div>
 </footer>
