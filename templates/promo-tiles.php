bbd5b649063e21669e6122df97ab297173aa7c55
Szabó Gábor <szabogabi@gmail.com>
2017-10-06 14:03:10 +0200

    promotiles

--- templates/promo-tiles.php
+++ templates/promo-tiles.php
@@ -1,25 +1,47 @@
 <section class="promotiles">
-    <div class="grid-container full">
-        <div class="grid-x">
-            <div class="large-6 cell">
-                <div class="promotile promotile--large">
-                    Nagy promo
-                </div>
+    <div class="promotile promotile--large">
+        <figure class="promotile__bgimage">
+            <a href="#">
+                <img src="http://lorempixel.com/1200/800" alt="">
+            </a>
+        </figure>
+        <div class="promotile__inner">
+            <p class="promotile__subtitle">Nyolcadikos vagy?</p>
+            <h3 class="promotile__title">Tanulj menő szakgimnáziumban</h3>
+            <div class="promotile__lead">
+                <p>Szakmai érettségire készítünk fel 9-12. évfolyamon és biztos alapokat nyújtunk tovább tanuláshoz.</p>
             </div>
-            <div class="large-6 cell">
-                <div class="grid-x">
-                    <div class="small-6 cell">
-                        <div class="promotile">
-                            kis promo I
-                        </div>
-                    </div>
-                    <div class="small-6 cell">
-                        <div class="promotile">
-                            kis promo II.
-                        </div>
-                    </div>
-                </div>
+            <a href="#" class="promotile__action button small warning">Miért a Teleki?</a>
+        </div>
+    </div>
+    <div class="promotile">
+        <figure class="promotile__bgimage">
+            <a href="<?php the_permalink(6); ?>">
+                <img src="http://lorempixel.com/800/1200" alt="">
+            </a>
+        </figure>
+        <div class="promotile__inner">
+            <p class="promotile__subtitle">Már van érettségid?</p>
+            <h3 class="promotile__title">Szerezz OKJ szakmát ingyen</h3>
+            <div class="promotile__lead">
+                <p>OKJ-s ügyintézői és könyvelői képzéseinkkel jól fizetett közgazdasági területen helyezkedhetsz el.</p>
             </div>
+            <a href="<?php the_permalink(6); ?>" class="promotile__action button tiny alert">Tovább a szaképzésre</a>
+        </div>
+    </div>
+    <div class="promotile">
+        <figure class="promotile__bgimage">
+            <a href="<?php the_permalink(6); ?>">
+                <img src="http://lorempixel.com/800/1199" alt="">
+            </a>
+        </figure>
+        <div class="promotile__inner">
+            <p class="promotile__subtitle">Nem csak játék és szórakozás</p>
+            <h3 class="promotile__title">Telekisek hétköznapjai</h3>
+            <div class="promotile__lead">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, modi quidem dignissimos voluptatum officiis tenetur fugit illum tempora.</p>
+            </div>
+            <a href="<?php the_permalink(55); ?>" class="promotile__action button tiny secondary">Less be hozzánk</a>
         </div>
     </div>
 </section>
