e23e821c53e57d86680085cab7b6d5ed41817dd4
Szabó Gábor <szabogabi@gmail.com>
2017-10-12 12:05:17 +0200

    typo

--- single-kepzes.php
+++ single-kepzes.php
@@ -3,13 +3,11 @@
     <header class="kepzeshead ">
         <figure class="kepzeshead__fig">
             <div class="kepzeshead__imgwrap">
-                <?php if (get_field(fejleckep)) :?>
+                <?php if ( get_field('fejleckep') ) :?>
                     <img class="kepzeshead__img" src="<?= get_field('fejleckep')  ?>" alt="<?php the_title(); ?>">
                 <?php else: ?>
                     <img class="kepzeshead__img" src="<?= get_stylesheet_directory_uri().'/dist/images/nyitolap.jpg' ?>" alt="<?php the_title(); ?>">
-                <? endif; ?>
-
-                <!-- <img class="kepzeshead__img" src="//placekitten.com/1280/400" alt=""> -->
+                <?php endif; ?>
             </div>
         </figure>
         <div class="grid-container">
