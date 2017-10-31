d33643399eb991471c23c8dc7c6c8e275bf3267b
Szabó Gábor <szabogabi@gmail.com>
2017-10-31 16:38:50 +0100

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,25 +1,24 @@
 <footer class="sitefooter" role="contentinfo">
     <div class="sitefooter__main">
         <div class="grid-container">
-            <div class="grid-x grid-margin-x grid-margin-y medium-up-2 large-up-2">
-                <div class="cell">
+            <div class="grid-x grid-margin-x">
+                <div class="cell tablet-6 large-shrink">
                     <p class="ficonized">
                         <svg class="icon"><use xlink:href="#icon-logo"></use></svg>
-                        <strong>BGSZC Teleki Blanka Közgazdasági Szakgimnáziuma</strong><br>
-                        1095 Budapest, Mester u. 23.
+                        <strong class="fupper">BGSZC Teleki Blanka Közgazdasági Szakgimnáziuma</strong><br>
+                        H-1095 Budapest, Mester u. 23. &middot; OM azonosító: 203061
                     </p>
-                    <p>
-                        E-mail: <a href="mailto:titkarsag@teleki-bp.sulinet.hu">titkarsag@teleki-bp.sulinet.hu</a><br>
-                        Telefon:&nbsp;<a href="tel:+3612159254">+36 (1) 2159 254</a>
-                    </p>
-                    <p>OM azonosító: 203061</p>
-                    <p><a class="readmore widget__readmore" href="<?= get_permalink( 10 ) ?>">Térkép és részletek</a></p>
+                </div>
+                <div class="cell tablet-6 large-auto fcopy">
+                    <p><strong class="fupper">&copy; <?= date('Y') ?> Teleki &middot; Minden jog fenntartva! &middot; <a href="#">Jogi nyilatkozat</a> &middot; <a href="#">BGSZC</a></strong><br>
+                    <strong>E-mail:</strong> <a href="mailto:titkarsag@teleki-bp.sulinet.hu">titkarsag@teleki-bp.sulinet.hu</a> &middot; <strong>Telefon:</strong>&nbsp;<a href="tel:+3612159254">+36&nbsp;(1)&nbsp;2159&nbsp;254</a> &middot; <a class="fsocial" href="https://www.facebook.com/TelekiBlankaKozgazdasagiSzki/"><svg class="icon"><use xlink:href="#icon-facebook"></use></svg></a></p>
+
                 </div>
                 <?php dynamic_sidebar('sidebar-footer'); ?>
             </div>
         </div>
     </div>
-    <div class="sitefooter__sub">
+<!--     <div class="sitefooter__sub">
         <div class="grid-container">
             <div class="grid-x grid-margin-x">
                 <div class="cell atext-right">
@@ -27,5 +26,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </footer>
