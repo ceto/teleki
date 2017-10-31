03606b4299ec4a3c65256cb110a455750723f2c2
Szabó Gábor <szabogabi@gmail.com>
2017-10-31 17:33:21 +0100

    footerfix

--- templates/footer.php
+++ templates/footer.php
@@ -2,14 +2,14 @@
     <div class="sitefooter__main">
         <div class="grid-container">
             <div class="grid-x grid-margin-x">
-                <div class="cell tablet-6 large-shrink">
+                <div class="cell small-12 large-shrink text-center large-text-left">
                     <p class="ficonized">
                         <svg class="icon"><use xlink:href="#icon-logo"></use></svg>
                         <strong class="fupper">BGSZC Teleki Blanka Közgazdasági Szakgimnáziuma</strong><br>
                         H-1095 Budapest, Mester u. 23. &middot; OM azonosító: 203061
                     </p>
                 </div>
-                <div class="cell tablet-6 large-auto fcopy">
+                <div class="cell small-12 large-auto text-center large-text-right fcopy">
                     <p><strong class="fupper">&copy; <?= date('Y') ?> Teleki &middot; Minden jog fenntartva! &middot; <a href="#">Jogi nyilatkozat</a> &middot; <a href="#">BGSZC</a></strong><br>
                     <strong>E-mail:</strong> <a href="mailto:titkarsag@teleki-bp.sulinet.hu">titkarsag@teleki-bp.sulinet.hu</a> &middot; <strong>Telefon:</strong>&nbsp;<a href="tel:+3612159254">+36&nbsp;(1)&nbsp;2159&nbsp;254</a> &middot; <a class="fsocial" href="https://www.facebook.com/TelekiBlankaKozgazdasagiSzki/"><svg class="icon"><use xlink:href="#icon-facebook"></use></svg></a></p>
 
