498ac9b256d5e003470d60fa5c7a4fa656478cd4
Szabó Gábor <szabogabor@hydrogene.hu>
2017-12-07 12:21:21 +0100

    calendar

--- template-naptar.php
+++ template-naptar.php
@@ -15,7 +15,7 @@
                         <?php the_excerpt(); ?>
                     </div>
                 </header>
-                <p><a target="_blank" href="https://calendar.google.com/calendar/embed?src=hu.hungarian%23holiday%40group.v.calendar.google.com&ctz=Europe/Budapest" class="button small">Új ablakba</a> <a href="https://calendar.google.com/calendar/ical/hu.hungarian%23holiday%40group.v.calendar.google.com/public/basic.ics" class="button small">iCal</a></p>
+                <p><a target="_blank" href="https://calendar.google.com/calendar/embed?src=telekicalendar%40gmail.com&ctz=Europe%2FBudapest" class="button small">Új ablakba</a> <a href="https://calendar.google.com/calendar/ical/telekicalendar%40gmail.com/public/basic.ics" class="button small">iCal</a></p>
 
             </div>
         </div>
@@ -25,7 +25,9 @@
     <div class="grid-container">
         <div class="grid-x grid-margin-x align-center">
             <div class="cell">
-                <iframe class="gcal" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=hu&amp;bgcolor=%23FCFCFC&amp;src=hu.hungarian%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FBudapest" style="border-width:0" frameborder="0" scrolling="no"></iframe>
+                <iframe src="https://calendar.google.com/calendar/embed?src=telekicalendar%40gmail.com&ctz=Europe%2FBudapest" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+                <iframe class="gcal" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=hu&amp;bgcolor=%23FCFCFC&amp;src=telekicalendar%40gmail.com&amp;color=%23125A12&amp;ctz=Europe%2FBudapest" style="border-width:0" frameborder="0" scrolling="no"></iframe>
             </div>
         </div>
     </div>
