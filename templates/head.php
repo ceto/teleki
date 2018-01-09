fd565069006186eb7515a46f676eaf03a5930eac
Szabó Gábor <szabogabi@gmail.com>
2018-01-09 14:00:50 +0100

    favicon lib

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/favicon-16x16.png">
-    <link rel="manifest" href="<?= get_stylesheet_directory_uri(); ?>/lib/favicons/manifest.json">
+    <link rel="manifest" href="<?= get_stylesheet_directory_uri(); ?>/lib/manifest.json">
     <link rel="mask-icon" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/safari-pinned-tab.svg" color="#116d6a">
     <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/favicon.ico">
     <meta name="msapplication-config" content="<?= get_stylesheet_directory_uri(); ?>/lib/browserconfig.xml">
