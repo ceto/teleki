c56ba0f6a97131bf677f0667a76e275a8224676a
Szabó Gábor <szabogabi@gmail.com>
2018-01-09 13:56:37 +0100

    favicons added

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,14 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?= get_stylesheet_directory_uri(); ?>/lib/favicons/manifest.json">
+    <link rel="mask-icon" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/safari-pinned-tab.svg" color="#116d6a">
+    <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicons/favicon.ico">
+    <meta name="msapplication-config" content="<?= get_stylesheet_directory_uri(); ?>/lib/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 </head>
